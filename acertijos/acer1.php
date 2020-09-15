ffc8d5852f723c00c7988fcd1cf6f742c2c79e5d
Miguel Barraza <correo@miguelbarraza.com.ar>
2020-09-15 18:51:05 -0300

    agregado una template

--- acertijos/acer1.php
+++ acertijos/acer1.php
@@ -1,20 +1,9 @@
-﻿<!DOCTYPE html>
-<html lang="es">
-<head>
-<meta charset="UTF-8">
-<title>PRIMER PROBLEMA</title>
-</head>
-<body>
-<h1>Resuelbe el primer desafío</h1>
-<br>
-<p>Si Pablo tiene 10 años más que el doble de la edad de Santiago; y ambas edades suman 40 años: ¿Cuantos años tiene Juan?</p>
-<br>
-<form action="resacertijos.php" method="post">
-<label>
-Respuesta:
-<input type="text" name="res1" placeholder="Escribe tu respuesta">
-</label>
-<input type="submit" value="Enviar respuesta">
-</form>
-</body>
-</html>
+﻿<?php
+$titulo = "PRIMER PROBLEMA";
+$problema = "Si Pablo tiene 10 años más que el doble de la edad de Santiago; y ambas edades suman 40 años: ¿Cuantos años tiene Juan?";
+
+$codigo = file_get_contents("consigna.html");
+$codigo = str_replace("{titulo}", $titulo, $codigo);
+$codigo = str_replace("{consigna}", $problema, $codigo);
+print($codigo);
+?>
